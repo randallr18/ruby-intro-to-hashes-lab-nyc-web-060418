b06c414fe89a6ec05d7467f5eb89307840cb8d4e
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 15:48:49 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,16 @@ dictionary = {}
 end
 
 def actor
-
+address_book = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {railroad: {}}
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+
 	
 end
 
